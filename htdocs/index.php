4aa61acc0d1d68ef9c4d0070f2afcc2c5b10c2a3
sxry318 <sxyr318@gmail.com>
2015-07-09 00:31:29 +0900

    消えたのでコミット

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,23 +1,25 @@
 <?php
-// ライブラリの読み込み
-require '../vendor/autoload.php';
+// セッション開始
+session_start();
 
-// 設定ファイルの読み込み
+// ライブラリ読み込み
+require __DIR__.'/../vendor/autoload.php';
+
+// 設定ファイル読み込み
 require __DIR__.'/../config.php';
 
-// データベース接続のセットアップ
+// DB接続セットアップ
 \Base\DB::registerIlluminate($db_settings);
 
-// Slimの初期化
+// Slim初期化
 $app = new \Slim\Slim([
-	//'templates.path' => TEMPLATES_DIR_PATH,
-	//'view' => new \Slim\Views\Twig()
+	'templates.path' => TEMPLATES_DIR_PATH,
+	'view' => new \Slim\Views\Twig(),
+	'debug' => DEBUG,
 ]);
 
 // Slimにルートを登録
-$app->get('/', function() {
-	echo "Hello, World!";
-});
+\Tinitter\Route::registration($app);
 
 // 実行
 $app->run();
